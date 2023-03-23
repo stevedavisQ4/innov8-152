44975ab76f255352f887bee361b07211870029a8
Steve Davis <steve.davis@q4inc.com>
2023-03-23 12:47:29 -0400

    nightwatch

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,10 @@ const openai = new OpenAIApi(configuration);
 
 app.get('/', async (req, res) => {
     const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "You are a brilliant Software Developer.\n\nWrite me a Nightwatch tests for the following scenario: \n- Navigate to the Facebook login page\n- Enter valid username and password\n- Click the “Login” button\n- Verify that the user is successfully logged in\n\n//Test: Login\nmodule.exports = {\n  'Navigate to the Facebook login page': function (browser) {\n    browser\n      .url('https://www.facebook.com/')\n      .waitForElementVisible('body', 1000)\n      .assert.title('Facebook - Log In or Sign Up')\n  },\n  'Enter valid username and password': function (browser) {\n    browser\n      .setValue('#email', 'test@example.com')\n      .setValue('#pass', 'testpass')\n  },\n  'Click the “Login” button': function (browser) {\n    browser\n      .click('#u_0_b')\n  },\n  'Verify that the user is successfully logged in': function (browser) {\n    browser\n      .waitForElementVisible('#userNavigationLabel', 1000)\n      .end();\n  }\n};",
+        model: "text-davinci-002",
+        prompt: "You are world-class software engineer.\n\nWrite a test in Nightwatch for the following scenario:\nTest Scenario: Netflix IR Website\n\nObjective: To ensure that the Netflix IR website is working as expected and all its features are functional.\n\nPre-requisites:\n\nA web browser (e.g., Chrome, Firefox)\nA stable internet connection\nTest account credentials\nSteps:\n1. Open the web browser and navigate to the Netflix IR website: https://ir.netflix.net/.\n2. Verify that the website's homepage is displayed with the latest news and events related to Netflix.\n\n",
         temperature: 0.7,
-        max_tokens: 256,
+        max_tokens: 8000,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
