81e61d25e7484e1d6e10bd7816d6d23d47ece395
Emmanuel Kiametis <emmanuel.kiametis@q4inc.com>
2023-03-23 14:10:10 -0400

    fix: Adding try / catch

--- src/app.ts
+++ src/app.ts
@@ -12,23 +12,30 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 app.get("/", async (req, res) => {
-  const response = await openai.createCompletion({
-    model: config.MODEL,
-    prompt:
-      "You are world-class software engineer.\n\nWrite a test in Nightwatch for the following scenario:\nTest Scenario: Netflix IR Website\n\nObjective: To ensure that the Netflix IR website is working as expected and all its features are functional.\n\nPre-requisites:\n\nA web browser (e.g., Chrome, Firefox)\nA stable internet connection\nTest account credentials\nSteps:\n1. Open the web browser and navigate to the Netflix IR website: https://ir.netflix.net/.\n2. Verify that the website's homepage is displayed with the latest news and events related to Netflix.\n\n",
-    temperature: 0.7,
-    max_tokens: 2200,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  });
-
-  const time = new Date().getTime();
-  fs.writeFileSync(`tests/${time}.js`, response.data.choices[0].text);
-
-  res.status(200).send({
-    bot: response.data.choices[0].text,
-  });
+  try {
+    const response = await openai.createCompletion({
+      model: config.MODEL,
+      prompt:
+        "You are world-class software engineer.\n\nWrite a test in Nightwatch for the following scenario:\nTest Scenario: Netflix IR Website\n\nObjective: To ensure that the Netflix IR website is working as expected and all its features are functional.\n\nPre-requisites:\n\nA web browser (e.g., Chrome, Firefox)\nA stable internet connection\nTest account credentials\nSteps:\n1. Open the web browser and navigate to the Netflix IR website: https://ir.netflix.net/.\n2. Verify that the website's homepage is displayed with the latest news and events related to Netflix.\n\n",
+      temperature: 0.7,
+      max_tokens: 2200,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+  
+    const time = new Date().getTime();
+    fs.writeFileSync(`tests/${time}.js`, response.data.choices[0].text);
+  
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch(err) {
+    console.error(err);
+    res.status(400).send({
+      err,
+    });
+  }
 });
 
 app.listen(port, () => {
