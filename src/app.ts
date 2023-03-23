98316fe5c58a25792f8812abe8e0878876c3deb9
Charles Tapar <charles.t@live.ca>
2023-03-23 14:02:56 -0400

    Auto stash before merge of "main" and "origin/main"

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
 import express from "express";
 import { Configuration, OpenAIApi } from "openai";
 import fs from "fs";
+import * as config from "./config";
 
 const app = express();
 const port = 3000;
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: config.API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 app.get("/", async (req, res) => {
   const response = await openai.createCompletion({
-    model: "text-davinci-003",
+    model: config.MODEL,
     prompt:
       "You are world-class software engineer.\n\nWrite a test in Nightwatch for the following scenario:\nTest Scenario: Netflix IR Website\n\nObjective: To ensure that the Netflix IR website is working as expected and all its features are functional.\n\nPre-requisites:\n\nA web browser (e.g., Chrome, Firefox)\nA stable internet connection\nTest account credentials\nSteps:\n1. Open the web browser and navigate to the Netflix IR website: https://ir.netflix.net/.\n2. Verify that the website's homepage is displayed with the latest news and events related to Netflix.\n\n",
     temperature: 0.7,
