424f06afdc6619893044b0497cdd3720a67f1f24
Charles Tapar <charles.t@live.ca>
2023-03-23 13:22:27 -0400

    pushe marie

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,29 @@
-import express from 'express';
-import { Configuration, OpenAIApi } from 'openai';
+import express from "express";
+import { Configuration, OpenAIApi } from "openai";
 
 const app = express();
 const port = 3000;
 
 const configuration = new Configuration({
-    apiKey: "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",
+  // Compromised API Key: use new one
+  apiKey: "fake",
 });
 const openai = new OpenAIApi(configuration);
 
-app.get('/', async (req, res) => {
-    const response = await openai.createCompletion({
-        model: "text-davinci-002",
-        prompt: "You are world-class software engineer.\n\nWrite a test in Nightwatch for the following scenario:\nTest Scenario: Netflix IR Website\n\nObjective: To ensure that the Netflix IR website is working as expected and all its features are functional.\n\nPre-requisites:\n\nA web browser (e.g., Chrome, Firefox)\nA stable internet connection\nTest account credentials\nSteps:\n1. Open the web browser and navigate to the Netflix IR website: https://ir.netflix.net/.\n2. Verify that the website's homepage is displayed with the latest news and events related to Netflix.\n\n",
-        temperature: 0.7,
-        max_tokens: 8000,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-    });
+app.get("/", async (req, res) => {
+  // const response = await openai.createCompletion({
+  //   model: "text-davinci-002",
+  //   prompt:
+  //     "You are world-class software engineer.\n\nWrite a test in Nightwatch for the following scenario:\nTest Scenario: Netflix IR Website\n\nObjective: To ensure that the Netflix IR website is working as expected and all its features are functional.\n\nPre-requisites:\n\nA web browser (e.g., Chrome, Firefox)\nA stable internet connection\nTest account credentials\nSteps:\n1. Open the web browser and navigate to the Netflix IR website: https://ir.netflix.net/.\n2. Verify that the website's homepage is displayed with the latest news and events related to Netflix.\n\n",
+  //   temperature: 0.7,
+  //   max_tokens: 8000,
+  //   top_p: 1,
+  //   frequency_penalty: 0,
+  //   presence_penalty: 0,
+  // });
 
-    console.log(response);
-    res.send("hello world");
+  // console.log(response);
+  res.send("hello world test");
 });
 
 app.listen(port, () => {
