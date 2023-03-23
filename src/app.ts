32209577182a58682f548e03efe70c170fd9135a
Steve Davis <steve.davis@q4inc.com>
2023-03-23 13:18:37 -0400

    remove api key, set max tokens

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,16 @@ const app = express();
 const port = 3000;
 
 const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: "SOME_TOKEN",
 });
 const openai = new OpenAIApi(configuration);
 
 app.get('/', async (req, res) => {
     const response = await openai.createCompletion({
-        model: "text-davinci-002",
+        model: "text-davinci-003",
         prompt: "You are world-class software engineer.\n\nWrite a test in Nightwatch for the following scenario:\nTest Scenario: Netflix IR Website\n\nObjective: To ensure that the Netflix IR website is working as expected and all its features are functional.\n\nPre-requisites:\n\nA web browser (e.g., Chrome, Firefox)\nA stable internet connection\nTest account credentials\nSteps:\n1. Open the web browser and navigate to the Netflix IR website: https://ir.netflix.net/.\n2. Verify that the website's homepage is displayed with the latest news and events related to Netflix.\n\n",
         temperature: 0.7,
-        max_tokens: 8000,
+        max_tokens: 2200,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
